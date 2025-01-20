402cb353e9151fc112fd3299ed6474cd97031397
陈阳 <1178849864@qq.com>
2025-01-20 14:46:31 +0800

    通过请求上下文操作header

--- GrpcTestService/Services/DemoService.cs
+++ GrpcTestService/Services/DemoService.cs
@@ -6,10 +6,20 @@ namespace GrpcTestService.Services;
 
 public class DemoService :Demo.DemoBase
 {
-    public override Task<GetCustomerByIdResponse> GetCustomerById(GetCustomerByIdRequest request, ServerCallContext context)
+    public override async Task<GetCustomerByIdResponse> GetCustomerById(GetCustomerByIdRequest request, ServerCallContext context)
     {
+        //获取请求中的header
+        var head = context.GetHttpContext().Request.Headers["x"];
+
+        //写入响应的header
+        var metadata = new Metadata() { };
+        metadata.Add("y", "666");
+        await context.WriteResponseHeadersAsync(metadata);
+    
+        //通过解析后的token获取当前登陆人相关信息
+        var claims = context.GetHttpContext().User.Claims.ToArray();
         var res = new GetCustomerByIdResponse();
         res.Createtime = Timestamp.FromDateTime(DateTime.UtcNow);
-        return Task.FromResult(res);
+        return await Task.FromResult(res);
     }
 }
